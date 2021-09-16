beb401594517dde8d589d21d419b70aa6522abb5
Marc Hauschildt <marc.hauschildt@gmail.com>
2021-09-16 18:54:08 -0500

    demonstrated homework setup

--- src/com/hauschildt/SalesTransaction.java
+++ src/com/hauschildt/SalesTransaction.java
@@ -2,11 +2,105 @@ package com.hauschildt;
 
 import java.time.LocalDateTime;
 
-public class SalesTransaction {
+public class SalesTransaction implements Comparable<SalesTransaction> {
     private int transactionId;
     private int salesPersonId;
     private LocalDateTime transactionDateTime;
     private int itemId;
     private double unitPrice;
     private int quantitySold;
+
+    public static int DEFAULT_TRANSACTION_ID = 0;
+    public static int DEFAULT_SALESPERSON_ID = 0;
+    public static LocalDateTime DEFAULT_TRANSACTION_DATE_TIME = LocalDateTime.now();
+    public static int DEFAULT_Item_ID = 0;
+    public static double DEFAULT_UNIT_PRICE = 0;
+    public static int DEFAULT_QUANTITY_SOLD = 0;
+
+    public static int MIN_TRANSACTION_ID = 0;
+
+    public SalesTransaction() {
+        this(DEFAULT_TRANSACTION_ID, DEFAULT_SALESPERSON_ID, DEFAULT_TRANSACTION_DATE_TIME, DEFAULT_Item_ID, DEFAULT_UNIT_PRICE,DEFAULT_QUANTITY_SOLD);
+    }
+
+    public SalesTransaction(int transactionId, int salesPersonId, LocalDateTime transactionDateTime, int itemId, double unitPrice, int quantitySold) {
+        this.transactionId = transactionId;
+        this.salesPersonId = salesPersonId;
+        this.transactionDateTime = transactionDateTime;
+        this.itemId = itemId;
+        this.unitPrice = unitPrice;
+        this.quantitySold = quantitySold;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        validateTransactionId(transactionId);
+        this.transactionId = transactionId;
+    }
+
+    private void validateTransactionId(int transactionId) {
+        if(transactionId < MIN_TRANSACTION_ID) {
+            throw new IllegalArgumentException("Negative numbers are not allowed.");
+        }
+    }
+
+    public int getSalesPersonId() {
+        return salesPersonId;
+    }
+
+    public void setSalesPersonId(int salesPersonId) {
+        this.salesPersonId = salesPersonId;
+    }
+
+    public LocalDateTime getTransactionDateTime() {
+        return transactionDateTime;
+    }
+
+    public void setTransactionDateTime(LocalDateTime transactionDateTime) {
+        this.transactionDateTime = transactionDateTime;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQuantitySold() {
+        return quantitySold;
+    }
+
+    public void setQuantitySold(int quantitySold) {
+        this.quantitySold = quantitySold;
+    }
+
+    @Override
+    public String toString() {
+        return "SalesTransaction{" +
+                "transactionId=" + transactionId +
+                ", salesPersonId=" + salesPersonId +
+                ", transactionDateTime=" + transactionDateTime +
+                ", itemId=" + itemId +
+                ", unitPrice=" + unitPrice +
+                ", quantitySold=" + quantitySold +
+                '}';
+    }
+
+    @Override
+    public int compareTo(SalesTransaction o) {
+        return 0;
+    }
 }
---
+++ tests/com/hauschildt/SalesTransactionTest.java
@@ -0,0 +1,110 @@
+package com.hauschildt;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalesTransactionTest {
+
+    SalesTransaction salesTransaction;
+
+    @BeforeEach
+    void setUp() {
+        salesTransaction = new SalesTransaction();
+    }
+
+    @Test
+    void getTransactionId() {
+        int expected = SalesTransaction.DEFAULT_TRANSACTION_ID;
+        int actual = salesTransaction.getTransactionId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setTransactionIdZeroGood() {
+        salesTransaction.setTransactionId(0);
+        int expected = 0;
+        int actual = salesTransaction.getTransactionId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setTransactionIdPotiveGood() {
+        salesTransaction.setTransactionId(1);
+        int expected = 1;
+        int actual = salesTransaction.getTransactionId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setTransactionIdNegativeBad() {
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() {
+                salesTransaction.setTransactionId(SalesTransaction.MIN_TRANSACTION_ID - 1);
+            }
+        });
+    }
+
+    @Test
+    void getSalesPersonId() {
+        fail();
+    }
+
+    @Test
+    void setSalesPersonId() {
+        fail();
+    }
+
+    @Test
+    void getTransactionDateTime() {
+        fail();
+    }
+
+    @Test
+    void setTransactionDateTime() {
+        fail();
+    }
+
+    @Test
+    void getItemId() {
+        fail();
+    }
+
+    @Test
+    void setItemId() {
+        fail();
+    }
+
+    @Test
+    void getUnitPrice() {
+        fail();
+    }
+
+    @Test
+    void setUnitPrice() {
+        fail();
+    }
+
+    @Test
+    void getQuantitySold() {
+        fail();
+    }
+
+    @Test
+    void setQuantitySold() {
+        fail();
+    }
+
+    @Test
+    void testToString() {
+        fail();
+    }
+
+    @Test
+    void compareTo() {
+        fail();
+    }
+}
